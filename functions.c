586d325ba75e2fdd47f73e1a1150944dd1a70626
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-20 20:16:59 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,6 +3,7 @@
 /************************* PRINT CHAR *************************/
 
 /**
+ * print f project by faith and brian
  * print_char - Prints a char
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
