a7d4b46f15783b8a8318afd1a34103c4e0c53b4b
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-20 20:20:27 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * print f project by faith and brian
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
