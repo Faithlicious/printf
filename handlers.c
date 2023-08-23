27f8038e42c67f07247cfe66b11127529c33c11e
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-23 04:33:44 +0300

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -3,7 +3,6 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * printf project by faith and brian
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
