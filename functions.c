752a236e6bc9cfbec7aade648cabae46cc731988
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-23 04:32:28 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,7 +2,6 @@
 
 /**
  * print_char - Prints a char
- * printf project by faith and brian 
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
