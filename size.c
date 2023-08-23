902126023d6d273786d423acb421852cdaae498f
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-23 04:34:38 +0300

    Update size.c

--- size.c
+++ size.c
@@ -2,7 +2,6 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * printf project by faith and brian
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
