914c1f563298adf538565e5f720d1073f77761c1
brianngigi <138031117+brianngigi@users.noreply.github.com>
2023-08-23 04:32:51 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -2,7 +2,6 @@
 
 /**
  * get_flags - Calculates active flags
- * printf project  by faith  and brian
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
